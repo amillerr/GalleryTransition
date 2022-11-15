aa0bf45bf2461219193c84a570ebc8953cf5ab7c
Tom Miller <62253468+amillerr@users.noreply.github.com>
2022-11-15 20:26:07 +0300

    Initial Commit

---
+++ GalleryTransition/GalleryTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryTransitionApp.swift
+//  GalleryTransition
+//
+//  Created by Artem Axenov on 2022-11-15.
+//
+
+import SwiftUI
+
+@main
+struct GalleryTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
